608386dde022f2fff8a42b6e76bf1d93ab8c87dc
Oleg Kovalenko <kn1feR26rus@yandex.ru>
2021-06-25 15:39:03 +0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { IntroComponent } from './components/main/intro/intro.component';
+import { AboutComponent } from './components/main/about/about.component';
+import { InfoComponent } from './components/main/info/info.component';
+import { ReviewsComponent } from './components/main/reviews/reviews.component';
+import { ContactsComponent } from './components/main/contacts/contacts.component';
+import { AmenitiesComponent } from './components/main/amenities/amenities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    IntroComponent,
+    AboutComponent,
+    InfoComponent,
+    ReviewsComponent,
+    ContactsComponent,
+    AmenitiesComponent
   ],
   imports: [
     BrowserModule,
